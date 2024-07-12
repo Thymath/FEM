c51959f903b44d462bd6c41a14f8a8a477243af9
Thymath <fj1994bc@gmail.com>
2024-07-12 23:05:18 +0200

    Added new function for point + first test

--- src/point.rs
+++ src/point.rs
@@ -1,8 +1,15 @@
 use std::ops::Sub;
 
+#[derive(Debug, PartialEq)]
 pub struct Point<T> {
     pub x: T,
     pub y: T,
+}
+
+impl<T> Point<T> {
+    fn new(x: T, y: T) -> Self {
+        Point { x, y }
+    }
 }
 
 impl<T> Sub for Point<T>
@@ -16,5 +23,18 @@ where
             x: self.x - rhs.x,
             y: self.y - rhs.y,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn substraction_of_points() {
+        let first = Point::new(3, 2);
+        let second = Point::new(2, -1);
+        let result = Point::new(1, 3);
+        assert_eq!(result, first - second);
     }
 }
